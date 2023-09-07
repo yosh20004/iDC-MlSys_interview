3aac96c8d4ad604672130f78e2ddf7d1c1d46e06
fzb0316 <fzb0316@163.com>
2023-09-07 08:56:47 +0800

    commit run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ./your_team_name.exe 64 16 8 graph/1M_5M.txt embedding/1M_64.bin weight/W_64_16.bin weight/W_16_8.bin
+# ./your_name.exe 64 16 8 graph/1M_5M.txt embedding/1M_64.bin weight/W_64_16.bin weight/W_16_8.bin
 
 
-./your_team_name.exe 64 16 8 graph/1024_example_graph.txt embedding/1024.bin weight/W_64_16.bin weight/W_16_8.bin
+./your_name.exe 64 16 8 graph/1024_example_graph.txt embedding/1024.bin weight/W_64_16.bin weight/W_16_8.bin
 
