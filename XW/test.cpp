4d7d418b2d0164b832b7cd548f7e01d55c1f44a1
yosh20005 <you@example.com>
2025-07-18 14:35:21 +0800

    尝试对XW做了分块优化，明确了大体优化思路

--- XW/test.cpp
+++ XW/test.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <chrono>
 #include <cstring>
+#include <utility>
+#include <memory>
 
 #ifdef USE_BLAS
 #include <cblas.h>
@@ -10,8 +12,8 @@
 
 constexpr int M = 1024;   // 行
 constexpr int K = 64;     // 内
-constexpr int N = 16;     // 列
-constexpr int TIMES = 1000;
+constexpr int N = 1024;     // 列
+constexpr int TIMES = 300;
 
 using f32 = float;
 
@@ -21,48 +23,92 @@ T* alloc(int n) {
     for (int i = 0; i < n; ++i) p[i] = (T)(rand()) / RAND_MAX;
     return p;
 }
+struct AlignedDeleter {
+    void operator()(void* p) const {
+        free(p);  // aligned_alloc 使用 free 释放
+    }
+};
 
 
-template<bool Transpose = false>
-void gemm_kernel(const float *A_pack,
-                 const float *B_pack,
-                 float *C_block, 
+[[gnu::always_inline]] void gemm_kernel_4x4(const float* __restrict__ A_L1_local,
+                     const float* __restrict__ B_L1_local,
+                     float* __restrict__ C_block, 
+                     int ldc)
+{
+    // 4x4 子块乘法：A(4,4) * B(4,4) → C(4,4)
+    constexpr int stride = 4;
+    for (int i = 0; i < stride; ++i)
+        for (int j = 0; j < stride; ++j) {
+            float sum = 0.f;
+            for (int k = 0; k < stride; ++k) 
+                sum += A_L1_local[i * stride + k] * 
+                       B_L1_local[k * stride + j];
+            C_block[i * ldc + j] += sum;
+        }
+}
+
+
+void gemm_kernel(const float* __restrict__ A_pack,
+                 const float* __restrict__ B_pack,
+                 float* __restrict__ C_block, 
                  int ldc,       
                  int mc_real, 
                  int nc_real, 
                  int kc_real) {
+                    
+    constexpr int stride = 4; // 步长
+    auto A_L1_local = std::unique_ptr<f32[], AlignedDeleter>(alloc<f32>(stride * stride));
+    auto B_L1_local = std::unique_ptr<f32[], AlignedDeleter>(alloc<f32>(stride * stride));
 
     // 朴素子块乘法：A(mc,kc) * B(kc,nc) → C(mc,nc)
-    if constexpr (!Transpose) {
-        for (int i = 0; i < mc_real; ++i)
-            for (int j = 0; j < nc_real; ++j) {
-                float sum = 0.f;
-                for (int k = 0; k < kc_real; ++k) 
-                    sum += A_pack[i * kc_real + k] * 
-                           B_pack[k * nc_real + j];
-                C_block[i * ldc + j] += sum;
+    for (int i = 0; i < mc_real; i += stride)
+        for (int j = 0; j < nc_real; j += stride)
+            for (int k = 0; k < kc_real; k += stride) {
+                // load A_L1_local
+                for (int ii = 0; ii < stride; ++ii) {
+                    for (int jj = 0; jj < stride; ++jj) {
+                        A_L1_local[ii * stride + jj] = 
+                            A_pack[(i + ii) * kc_real + k + jj];
+                    }
+                }
+
+                // load B_L1_local
+                for (int ii = 0; ii < stride; ++ii) {
+                    for (int jj = 0; jj < stride; ++jj) {
+                        B_L1_local[ii * stride + jj] = 
+                            B_pack[(k + ii) * nc_real + j + jj];
+                    }
+                }
+
+                // 4*4 子块乘法
+                gemm_kernel_4x4(
+                                A_L1_local.get(), 
+                                B_L1_local.get(), 
+                                C_block + i * N + j, ldc);
             }
-        return;
-    }
-
-
-
 }
 
 // A(M,K) * B(K,N) → C(M,N)
 void gemm_test(const float *A, const float *B, float *C) {
-    constexpr uint cacheSize = 16;
-    f32* A_cache = alloc<f32>(cacheSize);
-    f32* B_cache = alloc<f32>(cacheSize);
+    constexpr int mc = 16;
+    constexpr int kc = 16;
+    constexpr int nc = 128;  
+    constexpr int A_cache_size = mc * kc;
+    constexpr int B_cache_size = kc * nc;
 
-    int mc = 1;
-    int kc = cacheSize / mc;
-    int nc = 1;  // mc = nc
+    static f32* A_cache = alloc<f32>(A_cache_size);
+    static f32* B_cache = alloc<f32>(B_cache_size);
 
-    for (int i = 0; i < M; i += mc) {
-        for (int j = 0; j < N; j += nc) {
-            for (int k = 0; k < K; k += kc) {
+    for (int j = 0; j < N; j += nc) {
+        for (int k = 0; k < K; k += kc) {
+            // load B_cache
+            for (int ii = 0; ii < kc; ++ii) {
+                for (int jj = 0; jj < nc; ++jj) {
+                    B_cache[ii * nc + jj] = B[(k + ii) * N + (j + jj)];
+                }
+            }
 
+            for (int i = 0; i < M; i += mc) {
                 // load A_cache
                 for (int ii = 0; ii < mc; ++ii) {
                     for (int kk = 0; kk < kc; ++kk) {
@@ -70,18 +116,9 @@ void gemm_test(const float *A, const float *B, float *C) {
                     }
                 }
 
-                // load B_cache
-                for (int ii = 0; ii < kc; ++ii) {
-                    for (int jj = 0; jj < nc; ++jj) {
-                        B_cache[ii * nc + jj] = B[(k + ii) * N + (j + jj)];
-                    }
-                }
-
-                // call gemm_kernel
                 gemm_kernel(A_cache, B_cache, 
-                           C + i * N + j, N, mc, nc, kc);
+                        C + i * N + j, N, mc, nc, kc);
             }
-            
         }
     }
 }
@@ -117,23 +154,30 @@ int main() {
     float *C2 = alloc<f32>(M * N);
     float *C3 = alloc<f32>(M * N);
 
+    {
+        // 预热
+        for (int i=0; i < 50; ++i) {
+            gemm_blas(A, B, C1); 
+            std::memset(C2, 0, M * N * sizeof(float));
+            gemm_test(A, B, C2); 
+        }
+    }
+
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int i=0; i < TIMES; ++i) {
         gemm_blas(A, B, C1);       // OpenBLAS
     }
     auto t2 = std::chrono::high_resolution_clock::now();
     for (int i=0; i < TIMES; ++i) {
-        gemm_test(A, B, C2);       // test
+        std::memset(C2, 0, M * N * sizeof(float));
+        gemm_test(A, B, C2);     // test
     }
     auto t3 = std::chrono::high_resolution_clock::now();
-    for (int i=0; i < TIMES; ++i) {
-        gemm_native(A, B, C3);     // native
-    }
-
-    {
-        std::memset(C2, 0, M * N * sizeof(float));
-        gemm_test(A, B, C2); 
-    }
+    // for (int i=0; i < TIMES; ++i) {
+    //     std::memset(C3, 0, M * N * sizeof(float));
+    //     //gemm_native(A, B, C3);     // native
+    // }
+    // auto t4 = std::chrono::high_resolution_clock::now();
 
     double err = 0;
     for (int i = 0; i < M * N; ++i)
@@ -141,10 +185,10 @@ int main() {
     std::printf("max diff = %.3e\n", err);
     std::printf("OpenBLAS = %.3f ms\n",
                 std::chrono::duration<double, std::milli>(t2 - t1).count());
-    std::printf("My impl  = %.3f ms\n",
+    std::printf("My impl1  = %.3f ms\n",
                 std::chrono::duration<double, std::milli>(t3 - t2).count());
-    std::printf("Native   = %.3f ms\n",
-                std::chrono::duration<double, std::milli>(t3 - t1).count());
+    // std::printf("Native   = %.3f ms\n",
+    //             std::chrono::duration<double, std::milli>(t4 - t3).count());
 
     free(A); free(B); free(C1); free(C2); free(C3);
     return 0;
