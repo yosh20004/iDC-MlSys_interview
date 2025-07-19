3f879898875808b57ff2ba7be66c8f5bc85db4c4
yosh20005 <2172622103@qq.com>
2025-07-19 23:55:11 +0800

    规范已有命名若干

--- XW/test.cpp
+++ XW/test.cpp
@@ -11,10 +11,10 @@
 #endif
 
 constexpr int M = 1024;   // 行
-constexpr int K = 1024;     // 内
+constexpr int K = 64;     // 内
 constexpr int N = 1024;     // 列
 constexpr int TIMES = 100;
-constexpr int NUM_THREADS = 8;
+constexpr int NUM_THREADS = 1;
 
 using f32 = float;
 
@@ -27,26 +27,26 @@ T* alloc(int n) {
 }
 
 template<uint strideX, uint strideY, uint strideZ>
-inline void gemm_kernel_Btransposed(
-                     const float* __restrict__ A_L1_local,
-                     const float* __restrict__ B_L1_local, //B需要被转置
-                     float* __restrict__ C_L1_local)
+inline void gemm_micro_kernel_Btransposed(
+                     const float* __restrict__ A_micro,
+                     const float* __restrict__ B_micro, //B需要被转置
+                     float* __restrict__ C_micro)
 {
     // 子块乘法：A(X,Y) * B(Z,Y) → C(X,Z)
     for (uint i = 0; i < strideX; ++i) {
         for (uint j = 0; j < strideZ; ++j) {
             float sum = 0.f;
             for (uint k = 0; k < strideY; ++k) 
-                sum += A_L1_local[i * strideY + k] * 
-                       B_L1_local[j * strideY + k];
-            C_L1_local[i * strideZ + j] += sum;
+                sum += A_micro[i * strideY + k] * 
+                       B_micro[j * strideY + k];
+            C_micro[i * strideZ + j] += sum;
         }
     }
 }
 
-void gemm_kernel(const float* __restrict__ A_pack,
-                 const float* __restrict__ B_pack,
-                 float* __restrict__ C_pack, 
+void gemm_L1_kernel(const float* __restrict__ A_L1_pack,
+                 const float* __restrict__ B_L1_pack,
+                 float* __restrict__ C_L1_pack, 
                  uint mc_real, 
                  uint nc_real, 
                  uint kc_real) {
@@ -62,43 +62,38 @@ void gemm_kernel(const float* __restrict__ A_pack,
     // 子块乘法：A(mc,kc) * B(kc,nc) → C(mc,nc)
     for (uint i = 0; i < mc_real; i += strideX)
         for (uint j = 0; j < nc_real; j += strideZ) {
-
             std::memset(C_L1_local, 0, sizeof(C_L1_local));
-            for (uint k = 0; k < kc_real; k += strideY) {
 
-                // load A_L1_local (Alocal左上角的坐标为(i, k))
-                // A_pack(X, Y) → Alocal(X, Y)
-                // A_pack[i + ii][k + jj] → Alocal[ii][jj]
+            for (uint k = 0; k < kc_real; k += strideY) {
+                // A_L1_pack[i + ii][k + jj] → Alocal[ii][jj]
                 for (uint ii = 0; ii < strideX; ++ii) {
                     for (uint jj = 0; jj < strideY; jj += 8) {
-                        __m256 A_L1_local_line = _mm256_loadu_ps(A_pack + (i + ii) * kc_real + k + jj);
+                        __m256 A_L1_local_line = _mm256_loadu_ps(A_L1_pack + (i + ii) * kc_real + k + jj);
                         _mm256_storeu_ps(A_L1_local + ii * strideY + jj, A_L1_local_line);
                     }
                 }
 
-                // load B_L1_local (Blocal左上角的坐标为(k, j))
-                // B_pack(Y, Z) → Blocal(Z, Y)
-                // B_pack[k + ii][j + jj] → Blocal[jj][ii]
+                // B_L1_pack[k + ii][j + jj] → Blocal[jj][ii]
                 for (uint ii = 0; ii < strideY; ++ii) {
                     for (uint jj = 0; jj < strideZ; ++jj) {
                         B_L1_local[jj * strideY + ii] = 
-                            B_pack[(k + ii) * nc_real + j + jj];
+                            B_L1_pack[(k + ii) * nc_real + j + jj];
                     }
                 }
 
-                // 微内核 子块乘法
-                gemm_kernel_Btransposed<strideX, strideY, strideZ>(
+                // 4*4 子块乘法
+                gemm_micro_kernel_Btransposed<strideX, strideY, strideZ>(
                                 A_L1_local, 
                                 B_L1_local, 
                                 C_L1_local);
             }
 
-            // 将 C_L1_local 写回 C_pack
+            // 写回 C_L1_pack
             for (uint ii = 0; ii < strideX; ++ii) {
-                __m128 C_L1_local_line = _mm_loadu_ps(C_L1_local + ii * strideZ);
-                __m128 C_pack_line = _mm_loadu_ps(C_pack + (i + ii) * nc_real + j);
-                C_L1_local_line = _mm_add_ps(C_L1_local_line, C_pack_line);
-                _mm_storeu_ps(C_pack + (i + ii) * nc_real + j, C_L1_local_line);
+                __m128 C_new = _mm_loadu_ps(C_L1_local + ii * strideZ);
+                __m128 C_origin = _mm_loadu_ps(C_L1_pack + (i + ii) * nc_real + j);
+                C_new = _mm_add_ps(C_origin, C_new);
+                _mm_storeu_ps(C_L1_pack + (i + ii) * nc_real + j, C_new);
             }
         }
 }
@@ -108,55 +103,55 @@ void gemm_test(const float *A, const float *B, float *C) {
     constexpr int mc = 64;
     constexpr int kc = 64;
     constexpr int nc = 64;  
-    constexpr int A_cache_size = mc * kc;
-    constexpr int B_cache_size = kc * nc;
-    constexpr int C_cache_size = mc * nc;
+    constexpr int A_L1_pack_size = mc * kc;
+    constexpr int B_L1_pack_size = kc * nc;
+    constexpr int C_L1_pack_size = mc * nc;
 
     omp_set_num_threads(NUM_THREADS);
 
     #pragma omp parallel
     {
-        f32* A_cache = alloc<f32>(A_cache_size);
-        f32* B_cache = alloc<f32>(B_cache_size);
-        f32* C_cache = alloc<f32>(C_cache_size);
+        f32* A_L1_pack = alloc<f32>(A_L1_pack_size);
+        f32* B_L1_pack = alloc<f32>(B_L1_pack_size);
+        f32* C_L1_pack = alloc<f32>(C_L1_pack_size);
 
         #pragma omp for schedule(static)
         for (int i = 0; i < M; i += mc) {
             for (int j = 0; j < N; j += nc) {
-                std::memset(C_cache, 0, C_cache_size * sizeof(f32));
+                std::memset(C_L1_pack, 0, C_L1_pack_size * sizeof(f32));
 
                 for (int k = 0; k < K; k += kc) {
-                    // load A_cache (左上角坐标为(i, k))
+                    // load A_L1_pack (左上角坐标为(i, k))
                     for (int ii = 0; ii < mc; ++ii) {
                         for (int jj = 0; jj < kc; jj += 8) {
-                            __m256 A_cache_line = _mm256_loadu_ps(A + (i + ii) * K + k + jj);
-                            _mm256_storeu_ps(A_cache + ii * kc + jj, A_cache_line);
+                            __m256 A_L1_pack_line = _mm256_loadu_ps(A + (i + ii) * K + k + jj);
+                            _mm256_storeu_ps(A_L1_pack + ii * kc + jj, A_L1_pack_line);
                         }
                     }
 
-                    // load B_cache (左上角坐标为(k, j))
+                    // load B_L1_pack (左上角坐标为(k, j))
                     for (int ii = 0; ii < kc; ++ii) {
                         for (int jj = 0; jj < nc; jj += 8) {
-                            __m256 B_cache_line = _mm256_loadu_ps(B + (k + ii) * N + j + jj);
-                            _mm256_storeu_ps(B_cache + ii * nc + jj, B_cache_line);
+                            __m256 B_L1_pack_line = _mm256_loadu_ps(B + (k + ii) * N + j + jj);
+                            _mm256_storeu_ps(B_L1_pack + ii * nc + jj, B_L1_pack_line);
                         }
                     }
 
-                    gemm_kernel(A_cache, B_cache, 
-                                C_cache, mc, nc, kc);
+                    gemm_L1_kernel(A_L1_pack, B_L1_pack, 
+                                   C_L1_pack, mc, nc, kc);
                 }
 
                 for (int ii = 0; ii < mc; ++ii) {
                     for (int jj = 0; jj < nc; ++jj) {
-                        C[(i + ii) * N + j + jj] = C_cache[ii * nc + jj];
+                        C[(i + ii) * N + j + jj] = C_L1_pack[ii * nc + jj];
                     }
                 }
             }
         }
 
-        free(A_cache);
-        free(B_cache);
-        free(C_cache);
+        free(A_L1_pack);
+        free(B_L1_pack);
+        free(C_L1_pack);
     }
 }
 
